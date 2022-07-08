d3a57ec7a5e63a8f2a38260fc1c61af653567d61
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-07-09 00:51:36 +0100

    upcoming heading and explore link

--- src/components/Upcoming.jsx
+++ src/components/Upcoming.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Box, Container, Heading, Link } from '@chakra-ui/react';
 
-const upcomingMeets = () => {
-  return <></>;
+const UpcomingMeets = () => {
+  return (
+    <Box mt="20">
+      <Container maxWidth="container.xl">
+        <Box
+          mb="7"
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Box>
+            <Heading alignItems="center" as="h3" size="lg">
+              Upcoming online events
+            </Heading>
+          </Box>
+
+          <Box>
+            <Link fontWeight="semibold" color="teal.500">
+              Explore more events
+            </Link>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
 };
 
-export default upcomingMeets;
+export default UpcomingMeets;
