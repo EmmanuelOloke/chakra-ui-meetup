8f83dba1d762f91b2d9c252f91aff5a77ed71eb4
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-07-09 00:49:45 +0100

    imported and used UpcomingMeets component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './components/Banner';
 import ItWorks from './components/ItWorks';
+import UpcomingMeets from './components/Upcoming';
 
 function App() {
   return (
     <>
       <Banner />
       <ItWorks />
+      <UpcomingMeets />
     </>
   );
 }
