2e86babae44191c0c33f34320a4a37f6e1403e88
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-07-08 22:13:27 +0100

    added some assets and styled ItWorks Grid

--- src/components/ItWorks.jsx
+++ src/components/ItWorks.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Box, Container, Heading } from '@chakra-ui/react';
+import { Box, Container, Heading, Grid, Image, Link } from '@chakra-ui/react';
+import joinGroup from '../assets/joinagroup.svg';
+import ticket from '../assets/ticket.svg';
+import startGroup from '../assets/start-group.svg';
 
 const ItWorks = () => {
   return (
@@ -12,6 +15,51 @@ const ItWorks = () => {
           Meet new people who share your interests through online and in-person
           events. It’s free to create an account.
         </Box>
+
+        <Grid templateColumns="repeat(3, 1fr)" gap="6">
+          <Box mx="auto" align="center">
+            <Image w="100%" boxSize="160px" src={joinGroup} alt="brand" />
+            <Heading as="h5" size="md">
+              <Link>
+                <Box color="blue.100" pb="4">
+                  Join a Group
+                </Box>
+              </Link>
+            </Heading>
+            <p>
+              Do what you love, meet others who love it, find your community.
+              The rest is history!
+            </p>
+          </Box>
+          <Box mx="auto" align="center">
+            <Image w="100%" boxSize="160px" src={ticket} alt="brand" />
+            <Heading as="h5" size="md">
+              <Link>
+                <Box color="blue.100" pb="4">
+                  Find a event
+                </Box>
+              </Link>
+            </Heading>
+            <p>
+              Events are happening on just about any topic you can think of,
+              from online gaming and photography to yoga and hiking.
+            </p>
+          </Box>
+          <Box mx="auto" align="center">
+            <Image w="100%" boxSize="160px" src={startGroup} alt="brand" />
+            <Heading as="h5" size="md">
+              <Link>
+                <Box color="blue.100" pb="4">
+                  Start a group
+                </Box>
+              </Link>
+            </Heading>
+            <p>
+              You don’t have to be an expert to gather people together and
+              explore shared interests.
+            </p>
+          </Box>
+        </Grid>
       </Container>
     </Box>
   );
