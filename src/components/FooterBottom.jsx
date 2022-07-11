3337a8750bdd32a12cf9ba56ae9907e1f149e306
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-07-11 18:36:40 +0100

    imported necessary assests and Chakra-UI component for FooterBottom

--- src/components/FooterBottom.jsx
+++ src/components/FooterBottom.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
-import { Box } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Divider,
+  List,
+  ListItem,
+  Link,
+  Image,
+} from '@chakra-ui/react';
+
+import iosAppstore from '../assets/ios-download.svg';
+import androidAppstore from '../assets/android-download.svg';
 
 const FooterBottom = () => {
   return <Box></Box>;
