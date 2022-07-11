da231ad1edc590f468669d8ee0d021476af7f206
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-07-11 17:43:39 +0100

    imported needed assets and chakra-ui components

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import { Box, Image, Container, Heading, Grid, Link } from '@chakra-ui/react';
+
+import Storyone from '../assets/story-one.jpg';
+import Storytwo from '../assets/story-two.jpg';
+import Storythree from '../assets/story-three.jpg';
+
 const Stories = () => {
   return <></>;
 };
