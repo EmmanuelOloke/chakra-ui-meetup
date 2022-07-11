1344a3b3a92b6c17d004868cbec117f0b705d0f7
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-07-11 17:16:08 +0100

    populated and styled Connected component

---
+++ src/components/Connected.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { Box, Container, Image, Heading } from '@chakra-ui/react';
+import deviceLeft from '../assets/device-left.webp';
+import meetupLogo from '../assets/meetup-logo.svg';
+import deviceRight from '../assets/device-right.webp';
+import iosAppstore from '../assets/ios-download.svg';
+import androidAppstore from '../assets/android-download.svg';
+
+const Connected = () => {
+  return (
+    <Box my="20">
+      <Container maxWidth="container.xl">
+        <Box
+          display="flex"
+          pos="relative"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Box
+            display={{
+              sm: 'none',
+              md: 'block',
+              lg: 'block',
+              xl: 'block',
+              base: 'none',
+            }}
+          >
+            <Image w="260px" src={deviceLeft}></Image>
+          </Box>
+
+          <Box position="relative" mx="auto" align="center">
+            <Image src={meetupLogo}></Image>
+            <Heading as="h6" size="md" my="7">
+              Stay connected, <br /> Download the app.
+            </Heading>
+            <Box display="flex">
+              <Image p="5" src={iosAppstore}></Image>
+              <Image p="5" src={androidAppstore}></Image>
+            </Box>
+          </Box>
+
+          <Box
+            display={{
+              sm: 'none',
+              md: 'block',
+              lg: 'block',
+              xl: 'block',
+              base: 'none',
+            }}
+          >
+            <Image src={deviceRight}></Image>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default Connected;
