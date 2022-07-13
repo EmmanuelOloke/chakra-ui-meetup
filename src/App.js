9ec7371c00b8e9327fae4ca08cf86dca65c5b916
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-07-13 02:21:27 +0100

    added background image

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import { Box, Image } from '@chakra-ui/react';
+
+import bgImage from './assets/bg.svg';
+
 import React from 'react';
 import Banner from './components/Banner';
 import ItWorks from './components/ItWorks';
@@ -10,13 +14,27 @@ import FooterBottom from './components/FooterBottom';
 function App() {
   return (
     <>
-      <Banner />
-      <ItWorks />
-      <Upcoming />
-      <Popular />
-      <Connected />
-      <Stories />
-      <FooterBottom />
+      <Box position="absolute" left="0" top="0" right="0">
+        <Banner />
+        <ItWorks />
+        <Upcoming />
+        <Popular />
+        <Connected />
+        <Stories />
+        <FooterBottom />
+      </Box>
+
+      <Box
+        position="relative"
+        top="0"
+        bottom="0"
+        right="0"
+        left="0"
+        height="100vh"
+        zIndex={-1}
+      >
+        <Image w="100%" m="auto" objectFit="cover" sizes="300" src={bgImage} />
+      </Box>
     </>
   );
 }
